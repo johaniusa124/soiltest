ced039492e2cbb433d8bd98b62e766b574174174
johaniusa124 <142925679+johaniusa124@users.noreply.github.com>
2026-05-18 14:25:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,6 @@ def get_data():
 def home():
     return "Weather API is running!"
 
-@app.route('/initapp', methonds=['GET'])
 def init_db():
     try:
         conn = get_db_connection()
@@ -95,14 +94,12 @@ def init_db():
         conn.close()
 
         print("Table created successfully")
-        return(1)
 
     except Exception as e:
         print("Error creating table:", e)
-        return(0)
 
 
 if __name__ == "__main__":
-    # init_db()
+    init_db()
     app.run(host='0.0.0.0', port=8080)
 
