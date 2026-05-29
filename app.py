6868a9bc705744efc19b980d245c8d4907a11101
johaniusa124 <142925679+johaniusa124@users.noreply.github.com>
2026-05-28 23:07:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,6 @@ def get_params():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-    ```python
-with app.app_context():
-    ParamsDB.__table__.drop(db.engine)
-    #ParamsDB.__table__.create(db.engine)
+    with app.app_context():
+        ParamsDB.__table__.drop(db.engine)
+        #ParamsDB.__table__.create(db.engine)
